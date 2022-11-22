852551ece5ecc832595bc184cdd63b770863b464
EzzatEsam <s-ezzat.eisawy@zewailcity.edu.eg>
2022-11-22 18:48:54 +0200

    .

---
+++ gui.py
@@ -0,0 +1,20 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication , QMainWindow ,QWidget , QLabel
+import sys
+
+class gui_window :
+    def __init__(self) -> None:
+        app = QApplication(sys.argv)
+        window = QMainWindow()
+        window.setWindowTitle('Zc path')
+        
+        window.setGeometry(200,200,1000,700)
+        window.show()
+        label1 = QLabel(window)
+        map_picture = QPixmap('map.png')
+        label1.setPixmap(map_picture)
+        window.setCentralWidget(label1)
+        self.window = window
+        # Start the event loop.
+        sys.exit(app.exec_())
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('why')
+from gui import *
+
+def main ():
+    print('hi there')
+    wnd = gui_window()
+
+
+
+if __name__ ==  "__main__"  :
+    main()
+
